df22e5460767becf08882c7d3d30d6b9992476d0
KoaLa1101 <rproba@list.ru>
2021-03-31 15:34:26 +0300

    deploy to heroku

---
+++ src/main/java/ru/itlab/controllers/OauthController.java
@@ -0,0 +1,15 @@
+package ru.itlab.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class OauthController {
+
+    @GetMapping("/login/oauth")
+    public String showOauthScreen(){
+        
+    }
+}
